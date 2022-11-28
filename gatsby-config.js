867acb85ac8b95414dadf8e52a11cd0d114ae414
kakin <kaisboulakhlas9@gmail.com>
2022-11-28 18:58:47 +0100

    config netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,29 +5,49 @@ module.exports = {
     author: `@kaisboulakhlas`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-sass", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": "1999"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/assets/images/icon.png"
-    }
-  }, "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/assets/images/"
-    },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }, 'gatsby-plugin-offline', `gatsby-plugin-netlify`],
+  plugins: [
+    "gatsby-plugin-netlify-cms", 
+    "gatsby-plugin-sass", 
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        "trackingId": "1999"
+      }
+    }, 
+    "gatsby-plugin-image", 
+    "gatsby-plugin-sitemap", 
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        "icon": "src/assets/images/icon.png"
+      }
+    }, 
+    "gatsby-transformer-remark", 
+    "gatsby-plugin-sharp", 
+    "gatsby-transformer-sharp", 
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/assets/images/"
+      },
+      __key: "images"
+    }, {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: "markdown",
+        path: "./content/services",
+      },
+    }, 
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
+      },
+      __key: "pages"
+    }, 
+    'gatsby-plugin-offline', 
+    "gatsby-plugin-netlify"
+],
 };
