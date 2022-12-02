8c6e809a77a05974c6b1be6f8a9c2892673da81a
kakin <kaisboulakhlas9@gmail.com>
2022-12-02 12:22:01 +0100

    styles skills section + collection skills

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -16,7 +16,7 @@ const About = () => {
   ]
 
   return (
-    <>
+    <div className='container'>
       <h2 className='head-text'>
         Mes <span>Services</span>
         <br />
@@ -24,21 +24,23 @@ const About = () => {
       </h2>
 
       <div id="services" className="app__profiles">
-        {servicesData.map((service, index) => (
-          <motion.div
-            whileInView={{ opacity: 1 }}
-            whileHover={{ scale: 1.1 }}
-            transition={{ duration: 0.5, type: 'tween' }}
-            className="app__profile-item"
-            key={service.frontmatter.titre + index}
-          >
-            <GatsbyImage image={getImage(service.frontmatter.serviceimage.childImageSharp.gatsbyImageData)} alt={service.frontmatter.titre}/>
-            <h2 className="bold-text" style={{ marginTop: 20 }}>{service.frontmatter.titre}</h2>
-            <p className="p-text" style={{ marginTop: 10 }}>{service.frontmatter.description}</p>
-          </motion.div>
-        ))}
+        {
+          servicesData.map((service, index) => (
+            <motion.div
+              whileInView={{ opacity: 1 }}
+              whileHover={{ scale: 1.1 }}
+              transition={{ duration: 0.5, type: 'tween' }}
+              className="app__profile-item"
+              key={service.frontmatter.titre + index}
+            >
+              <GatsbyImage image={getImage(service.frontmatter.serviceimage.childImageSharp.gatsbyImageData)} alt={service.frontmatter.titre}/>
+              <h2 className="bold-text" style={{ marginTop: 20 }}>{service.frontmatter.titre}</h2>
+              <p className="p-text" style={{ marginTop: 10 }}>{service.frontmatter.description}</p>
+            </motion.div>
+          ))
+        }
       </div>
-    </>
+    </div>
   )
 }
 
--- src/container/Project/Project.jsx
+++ src/container/Project/Project.jsx
@@ -2,52 +2,112 @@ import React from 'react'
 import { AiFillEye, AiFillGithub } from 'react-icons/ai'
 import { motion } from 'framer-motion'
 import { AppWrap } from '../../wrapper'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import './project.scss'
 import { useState } from 'react'
 import { useProjectsData } from '../../hooks/use-project'
+import { useEffect } from 'react';
 
 const Project = () => {
   const projectsData = useProjectsData();
-  console.log(projectsData);
+  const [projects, setProjects] = useState([]);
   const [activeFilter, setActiveFilter] = useState('Tous');
-  const [filterProject, setFilterProject] = useState();
-  const [animateCard, setAnimateCard] = useState({ y : 0, opacity: 1 })
-  const handleWorkFilter = (item) => {
+  const [filterProject, setFilterProject] = useState([]);
+  const [animateCard, setAnimateCard] = useState({ y : 0, opacity: 1 });
+  const types = ['Tous','UI/UX', 'Web', 'Mobile'];
+
+  const handleprojectFilter = (item) => {
     setActiveFilter(item);
     setAnimateCard([{ y: 100, opacity: 0 }]);
 
     setTimeout(() => {
       setAnimateCard([{ y: 0, opacity: 1 }]);
-      setFilterProject(projectsData.filter((work) => work.type.includes(item)));
+      if (item === 'Tous') {
+        setFilterProject(projects);
+      } else {
+        setFilterProject(projects.filter((project) => project.frontmatter.type.includes(item)));
+      }
     }, 500);
   }
 
+  useEffect(() => {
+      setProjects(projectsData);
+      setFilterProject(projectsData);
+  }, [projectsData]);
+
   return (
-    <>
+    <div className='container'>
       <h2 className='head-text'>
         Mes différents<span>Projets</span>
       </h2>
-      <div className="app__work-filter">
-        {['Tous','UI/UX', 'Site web', 'Mobile'].map((item, index) => (
-          <div 
-            key={index}
-            onClick={() => handleWorkFilter(item)}
-            className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
-          >
-            {item}
-          </div>
-        ))
+      <div id="projets" className="app__project-filter">
+        {
+          types?.map((item, index) => (
+            <div 
+              key={index}
+              onClick={() => handleprojectFilter(item)}
+              className={`app__project-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
+            >
+              {item}
+            </div>
+          ))
         }
       </div>
       <motion.div
         animate={animateCard}
         transition={{ duration: 0.5, delayChildren: 0.5 }}
-        className="app__work-portfolio"
+        className="app__project-portfolio"
       >
+        {
+          filterProject.map((project, index) => (
+            <div className="app__project-item app__flex" key={index}>
+              <div
+                className="app__project-img app__flex"
+              >
+                <GatsbyImage style={{ height: "100%" }} image={getImage(project.frontmatter.projectimage.childImageSharp.gatsbyImageData)} alt={project.frontmatter.title}/>
+                <motion.div
+                  whileHover={{ opacity: [0, 1] }}
+                  transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
+                  className="app__project-hover app__flex"
+                >
+                  <a href={project.frontmatter.projectlink} target="_blank" rel="noreferrer">
 
+                    <motion.div
+                      whileInView={{ scale: [0, 1] }}
+                      whileHover={{ scale: [1, 0.90] }}
+                      transition={{ duration: 0.25 }}
+                      className="app__flex"
+                    >
+                      <AiFillEye />
+                    </motion.div>
+                  </a>
+                  <a href={project.frontmatter.codelink} target="_blank" rel="noreferrer">
+                    <motion.div
+                      whileInView={{ scale: [0, 1] }}
+                      whileHover={{ scale: [1, 0.90] }}
+                      transition={{ duration: 0.25 }}
+                      className="app__flex"
+                    >
+                      <AiFillGithub />
+                    </motion.div>
+                  </a>
+                </motion.div>
+              </div>
+
+              <div className="app__project-content app__flex">
+                <h4 className="bold-text">{project.frontmatter.title}</h4>
+                <p className="p-text" style={{ marginTop: 10 }}>{project.frontmatter.description}</p>
+
+                <div className="app__project-tag app__flex">
+                  <p className="p-text" style={{ color: "#f68338" }}>#{project.frontmatter.tags.join(' #')}</p>
+                </div>
+              </div>
+            </div>
+          ))
+        }
       </motion.div>
-    </>
+    </div>
   )
 }
 
-export default Project
+export default AppWrap(Project, 'projets');
