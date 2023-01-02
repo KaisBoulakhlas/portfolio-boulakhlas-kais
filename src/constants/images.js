1316f7eb80f18a642f83d87e596c292163305496
kakin <kaisboulakhlas9@gmail.com>
2023-01-02 14:34:08 +0100

    Fix icon manifest

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,7 +1,11 @@
 import email from '../assets/images/email.png';
 import mobile from '../assets/images/mobile.png';
+import icon from '../assets/images/icon.png';
+import bgWhite from '../assets/images/bgWhite.png';
 
 export default {
   email,
   mobile,
+  bgWhite,
+  icon,
 };
