c1a2970e471c0c106683fa0d1c040e08988255f1
kakin <kaisboulakhlas9@gmail.com>
2023-01-11 20:09:01 +0100

    add back button at others pages

--- src/pages/mentions-legales.js
+++ src/pages/mentions-legales.js
@@ -1,10 +1,14 @@
+import { Link } from "gatsby"
 import * as React from "react"
 import { Seo } from "../components/Seo"
 
 const MentionsLegales = () => {
   return (
-    <h1 className="head-text">Mentions <span className="text-secondary-color">légales</span></h1>
-  )
+    <main className="app__flex">
+      <h1 className="head-text">Mentions <span className="text-secondary-color">légales</span></h1>
+      <Link to="/" className="button">Retour</Link>.
+    </main>
+    )
 }
 
 export default MentionsLegales
--- src/pages/politique-de-confidentialite.js
+++ src/pages/politique-de-confidentialite.js
@@ -1,9 +1,13 @@
+import { Link } from "gatsby"
 import * as React from "react"
 import { Seo } from "../components/Seo"
 
 const PolitiqueConfidentialite = () => {
   return (
-    <h1 className="head-text">Politique de <span className="text-secondary-color">confidentialité</span></h1>
+    <main className="app__flex">
+      <h1 className="head-text">Politique de <span className="text-secondary-color">confidentialité</span></h1>
+      <Link to="/" className="button">Retour</Link>.
+    </main>
   )
 }
 
